970556d777635c6f22920583d15858f59bc896e5
Brian Oh <brijq91@gmail.com>
2018-11-05 17:05:09 +0800

    Routing Page Inclusion of new page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter as Router , Route} from 'react-router-dom'
+import {BrowserRouter as Router , Route, Switch, Link} from 'react-router-dom'
 import profilePage from './scenes/profilepage'
+import privacyPage from './scenes/privacypage'
+import projectsPage from './scenes/projectspage'
+import NotFound from './scenes/404page';
 import './styles/bootstrap.css'
 import './styles/oneui.css'
 
@@ -9,7 +12,12 @@ import './styles/oneui.css'
 ReactDOM.render((
     <Router>
       <div>
-        <Route path="/" component={profilePage}/>
+        <Switch>
+          <Route exact path="/" component={profilePage}/>
+          <Route exact path="/privacy" component={privacyPage}/>
+          <Route exact path="/projects" component={projectsPage}/>
+          <Route component={NotFound} />
+        </Switch>
       </div>
     </Router>
   ), document.getElementById('root')
