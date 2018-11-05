33fe7cdb13b1702cd5a1ae5514c276114aa52ca5
Brian Oh <brijq91@gmail.com>
2018-11-06 00:52:11 +0800

    Html Changes

--- src/scenes/privacypage/index.js
+++ src/scenes/privacypage/index.js
@@ -11,109 +11,118 @@ class privacyPage extends React.Component{
             <main className="main-container">
               <div className="hero">
                   <div className="hero-inner text-left">
-                      <div className="content content-full bg-white overflow-hidden">
+                      <div className="content content-full bg-light-gray overflow-hidden">
                           <div className="py-4">
-                              <h1 className="display-1 text-city" data-toggle="appear" data-className="animated flipInX">Privacy Policy</h1>
-                              <h4 className="h4 font-w300 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">Effective date: November 05, 2018</h4>
-                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">
-                                  My Site ("us", "we", or "our") operates the brijqtify.firebaseapp.com website (the "Service").<br/><br/>
+                              <div className="block">
+                                            <h1 className="block-header bg-city block-title display-1 text-black" data-toggle="appear" data-className="animated flipInX">Privacy Policy</h1>
+                                            <h4 className="h4 font-w300 text-muted mb-5 block-content experiencesector" data-toggle="appear" data-className="animated fadeInUp">Effective date: November 05, 2018</h4>
+                                            <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5 block-content experiencesector" data-toggle="appear" data-className="animated fadeInUp">
+                                                My Site ("us", "we", or "our") operates the brijqtify.firebaseapp.com website (the "Service").<br/><br/>
 
-                                  This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices<br/>
-                                  you have associated with that data. Our Privacy Policy for My Site is managed through Free Privacy Policy.
-                              </p>
-                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">
-                                  We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy.<br/>
-                                  Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible from<br/>
-                                  brijqtify.firebaseapp.com
-                              </p>
+                                                This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices<br/>
+                                                you have associated with that data. Our Privacy Policy for My Site is managed through Free Privacy Policy.
+                                            </p>
+                                            <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5 block-content experiencesector" data-toggle="appear" data-className="animated fadeInUp">
+                                                We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy.<br/>
+                                                Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible from<br/>
+                                                brijqtify.firebaseapp.com
+                                            </p>
+                              </div>
 
-                              <h1 className="display-1 text-city" data-toggle="appear" data-className="animated flipInX">Information Collection And Use</h1>
 
-                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">
-                                  We collect several different types of information for various purposes to provide and improve our Service to you.
-                              </p>
+                              <div className="block">
+                                              <h1 className="block-header bg-city block-title display-1 text-black" data-toggle="appear" data-className="animated flipInX">Information Collection And Use</h1>
 
-                              <h3 className="h3 font-w300 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">Types of Data Collected</h3>
+                                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5 block-content" data-toggle="appear" data-className="animated fadeInUp">
+                                                  We collect several different types of information for various purposes to provide and improve our Service to you.
+                                              </p>
 
-                              <h4 className="h4 font-w300 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">Personal Data</h4>
+                                              <h3 className="h3 font-w300 text-muted mb-5 block-content" data-toggle="appear" data-className="animated fadeInUp"><strong>Types of Data Collected</strong></h3>
 
-                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">
-                                  While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you ("Personal Data").<br/>
-                                  Personally identifiable information may include, but is not limited to:
-                              </p>
+                                              <h4 className="h4 font-w300 text-muted mb-5 block-content" data-toggle="appear" data-className="animated fadeInUp">Personal Data</h4>
 
-                              <ul>
-                                <li>Email address</li>
-                                <li>First name and last name</li>
-                                <li>Phone number</li>
-                                <li>Cookies and Usage Data</li>
-                              </ul>
+                                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5 block-content" data-toggle="appear" data-className="animated fadeInUp">
+                                                  While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you ("Personal Data").<br/>
+                                                  Personally identifiable information may include, but is not limited to:
+                                              </p>
 
-                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">
-                                    Examples of Cookies we use:
-                              </p>
+                                              <ul>
+                                                <li>Email address</li>
+                                                <li>First name and last name</li>
+                                                <li>Phone number</li>
+                                                <li>Cookies and Usage Data</li>
+                                              </ul>
 
-                              <ul>
-                                <li>Session Cookies. We use Session Cookies to operate our Service.</li>
-                                <li>Preference Cookies. We use Preference Cookies to remember your preferences and various settings.</li>
-                                <li>Security Cookies. We use Security Cookies for security purposes.</li>
-                              </ul>
+                                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5 block-content" data-toggle="appear" data-className="animated fadeInUp">
+                                                    Examples of Cookies we use:
+                                              </p>
 
-                              <h4 className="h4 font-w300 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">Use of Data</h4>
+                                              <ul>
+                                                <li>Session Cookies. We use Session Cookies to operate our Service.</li>
+                                                <li>Preference Cookies. We use Preference Cookies to remember your preferences and various settings.</li>
+                                                <li>Security Cookies. We use Security Cookies for security purposes.</li>
+                                              </ul>
 
-                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">
-                                  My Site uses the collected data for various purposes:
-                              </p>
+                                              <h4 className="h4 font-w300 text-muted mb-5 block-content" data-toggle="appear" data-className="animated fadeInUp">Use of Data</h4>
 
-                              <ul>
-                                <li>To provide and maintain the Service</li>
-                                <li>To notify you about changes to our Service</li>
-                                <li>To allow you to participate in interactive features of our Service when you choose to do so</li>
-                                <li>To provide customer care and support</li>
-                                <li>To provide analysis or valuable information so that we can improve the Service</li>
-                                <li>To monitor the usage of the Service</li>
-                                <li>To detect, prevent and address technical issues</li>
-                              </ul>
+                                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5 block-content" data-toggle="appear" data-className="animated fadeInUp">
+                                                  My Site uses the collected data for various purposes:
+                                              </p>
 
-                              <h3 className="h3 font-w300 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">Disclosure Of Data</h3>
+                                              <ul>
+                                                <li>To provide and maintain the Service</li>
+                                                <li>To notify you about changes to our Service</li>
+                                                <li>To allow you to participate in interactive features of our Service when you choose to do so</li>
+                                                <li>To provide customer care and support</li>
+                                                <li>To provide analysis or valuable information so that we can improve the Service</li>
+                                                <li>To monitor the usage of the Service</li>
+                                                <li>To detect, prevent and address technical issues</li>
+                                              </ul>
 
-                              <h4 className="h4 font-w300 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">Legal Requirements</h4>
+                                              <h3 className="h3 font-w300 text-muted mb-5 block-content" data-toggle="appear" data-className="animated fadeInUp"><strong>Disclosure Of Data</strong></h3>
 
-                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">
-                                  My Site may disclose your Personal Data in the good faith belief that such action is necessary to:
-                              </p>
+                                              <h4 className="h4 font-w300 text-muted mb-5 block-content" data-toggle="appear" data-className="animated fadeInUp">Legal Requirements</h4>
 
-                              <ul>
-                                <li>To comply with a legal obligation</li>
-                                <li>To protect and defend the rights or property of My Site</li>
-                                <li>To prevent or investigate possible wrongdoing in connection with the Service</li>
-                                <li>To protect the personal safety of users of the Service or the public</li>
-                                <li>To protect against legal liability</li>
-                              </ul>
+                                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5 block-content" data-toggle="appear" data-className="animated fadeInUp">
+                                                  My Site may disclose your Personal Data in the good faith belief that such action is necessary to:
+                                              </p>
 
-                              <h3 className="h3 font-w300 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">Security Of Data</h3>
+                                              <ul>
+                                                <li>To comply with a legal obligation</li>
+                                                <li>To protect and defend the rights or property of My Site</li>
+                                                <li>To prevent or investigate possible wrongdoing in connection with the Service</li>
+                                                <li>To protect the personal safety of users of the Service or the public</li>
+                                                <li>To protect against legal liability</li>
+                                              </ul>
 
-                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">
-                                  The security of your data is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. <br/>
-                                  While we strive to use commercially acceptable means to protect your Personal Data, we cannot guarantee its absolute security.
-                              </p>
+                                              <h3 className="h3 font-w300 text-muted mb-5 block-content" data-toggle="appear" data-className="animated fadeInUp"><strong>Security Of Data</strong></h3>
 
-                              <h3 className="h3 font-w300 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">Links To Other Sites</h3>
+                                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5 block-content lastsegmentprivacy" data-toggle="appear" data-className="animated fadeInUp">
+                                                  The security of your data is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. <br/>
+                                                  While we strive to use commercially acceptable means to protect your Personal Data, we cannot guarantee its absolute security.
+                                              </p>
+                              </div>
 
-                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">
-                                  Our Service may contain links to other sites that are not operated by us. If you click on a third party link, you will be directed to that third party s site. <br/>
-                                  We strongly advise you to review the Privacy Policy of every site you visit.<br/><br/>
+                              <div className="block">
+                                              <h3 className="block-header bg-city block-title display-1 text-black" data-toggle="appear" data-className="animated fadeInUp">Links To Other Sites</h3>
 
-                                  We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.
-                              </p>
+                                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5 block-content lastsegmentprivacy" data-toggle="appear" data-className="animated fadeInUp">
+                                                  Our Service may contain links to other sites that are not operated by us. If you click on a third party link, you will be directed to that third party s site. <br/>
+                                                  We strongly advise you to review the Privacy Policy of every site you visit.<br/><br/>
 
-                              <h3 className="h3 font-w300 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">Contact Us</h3>
+                                                  We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.
+                                              </p>
+                              </div>
 
-                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5" data-toggle="appear" data-className="animated fadeInUp">
-                                  If you have any questions about this Privacy Policy, please contact us:<br/>
-                                  <br/>
-                                  By email: brijq91@gmail.com
-                              </p>
+                              <div className="block">
+                                              <h3 className="block-header bg-city block-title display-1 text-black" data-toggle="appear" data-className="animated fadeInUp">Contact Us</h3>
+
+                                              <p className="privacyPagecontent font-w300 font-s13 text-muted mb-5 block-content lastsegmentprivacy" data-toggle="appear" data-className="animated fadeInUp">
+                                                  If you have any questions about this Privacy Policy, please contact us:<br/>
+                                                  <br/>
+                                                  By email: brijq91@gmail.com
+                                              </p>
+                              </div>
 
 
 
