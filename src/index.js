184bde41c07d89a313862aa10bcd0025c104844d
Brian Oh <brijq91@gmail.com>
2018-11-08 23:35:35 +0800

    Add Favicon for website

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Favicon from 'react-favicon';
 import {BrowserRouter as Router , Route, Switch, Link} from 'react-router-dom'
 import profilePage from './scenes/profilepage'
 import privacyPage from './scenes/privacypage'
@@ -7,11 +8,13 @@ import projectsPage from './scenes/projectspage'
 import NotFound from './scenes/404page';
 import './styles/bootstrap.css'
 import './styles/oneui.css'
+import profilePic from './images/favicon.png'
 
 
 ReactDOM.render((
     <Router>
       <div>
+        <Favicon url={profilePic}/>
         <Switch>
           <Route exact path="/" component={profilePage}/>
           <Route exact path="/privacy" component={privacyPage}/>
